dfb484274accf82ea7806a0b09b594345d83dac3
wybosys <wybosys@gmail.com>
2018-04-20 15:00:59 +0800

    sample

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,4 +1,4 @@
-import {App, BOOT, Hook, STARTED} from "../nnt/manager/app";
+import {App} from "../nnt/manager/app";
 import {Clusters} from "../nnt/manager/clusters";
 import {Config} from "../nnt/manager/config";
 
