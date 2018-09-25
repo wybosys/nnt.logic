c10e41ce918d0c9990019c903686e790b7bf8778
wybosys <wybosys@gmail.com>
2018-09-25 18:19:12 +0800

    prj

--- src/nnt/server/parser/bin.ts
+++ src/nnt/server/parser/bin.ts
@@ -1,1 +1,19 @@
+import {AbstractParser} from "./parser";
+import {FieldOption} from "../../core/proto";
 
+export class Bin extends AbstractParser {
+
+    checkInput(proto: any, params: any): number {
+        return 0;
+    }
+
+    // 根据属性定义解码数据
+    decodeField(fp: FieldOption, val: any, input: boolean, output: boolean): any {
+        return null;
+    }
+
+    // 将数据从参数集写入模型
+    fill(mdl: any, params: any, input: boolean, output: boolean): void {
+
+    }
+}
--- src/nnt/server/parser/parser.ts
+++ src/nnt/server/parser/parser.ts
@@ -1,9 +1,11 @@
 import {FieldOption} from "../../core/proto";
+import {logger} from "../../core/logger";
+import {Json} from "./json";
+import {Bin} from "./bin";
 
 /**
  * Paser负责将不同协议传输的数据回写刀模型中，根据不同的协议，params有时为json，有时是字节流
  */
-
 export abstract class AbstractParser {
 
     // 检查模型和输入数据的匹配情况，返回status的错误码
@@ -15,3 +17,21 @@ export abstract class AbstractParser {
     // 将数据从参数集写入模型
     abstract fill(mdl: any, params: any, input: boolean, output: boolean): void;
 }
+
+let parsers = new Map<string, AbstractParser>();
+
+export function RegisterParser(name: string, parser: AbstractParser) {
+    if (parsers.get(name)) {
+        logger.fatal("重复注册解析器{{=it.name}}", {name: name});
+        return;
+    }
+    parsers.set(name, parser);
+}
+
+export function FindParser(name: string): AbstractParser {
+    let r = parsers.get(name);
+    return r ? r : parsers.get("json");
+}
+
+RegisterParser("json", new Json());
+RegisterParser("bin", new Bin());
