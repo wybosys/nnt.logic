c1c99e857400d1367228ecca426a82b1302d4779
wybosys <wybosys@gmail.com>
2020-09-09 18:38:43 +0800

    dra

--- src/nnt/thirds/dra/signal-protocol/crypto.ts
+++ src/nnt/thirds/dra/signal-protocol/crypto.ts
@@ -1,4 +1,5 @@
 import crypto = require("crypto");
+import nacl = require("tweetnacl");
 import {BinaryLike} from "crypto";
 
 export class Crypto {
@@ -37,7 +38,11 @@ export class Crypto {
         return res.digest();
     }
 
-    static async HKDF(input: string, salt: string, info: Buffer): Promise<Buffer[]> {
+    static async HKDF(input: Buffer, salt: Buffer, info: Buffer): Promise<Buffer[]> {
+        if (salt.byteLength != 32) {
+            throw new Error("Got salt of incorrect length");
+        }
+
         // Specific implementation of RFC 5869 that only returns the first 3 32-byte chunks
         // XTODO: We dont always need the third chunk, we might skip it
         let PRK = Crypto.Sign(salt, input);
@@ -55,31 +60,51 @@ export class Crypto {
         return [T1, T2, T3];
     }
 
-    static async CreateKeyPair(prvkey?: Uint8Array): Promise<any> {
-        return null;
+    static CreateKeyPair(prvkey?: Buffer): nacl.BoxKeyPair {
+        if (!prvkey) {
+            prvkey = Crypto.GetRandomBytes(32);
+        }
+        return nacl.box.keyPair.fromSecretKey(prvkey);
     }
 
-    static async ECDHE(pubkey: Uint8Array, prvkey: Uint8Array): Promise<Uint8Array> {
-        return null;
+    static ECDHE(pubkey: Uint8Array, prvkey: Uint8Array): Uint8Array {
+        return nacl.box.before(pubkey, prvkey);
     }
 
-    static Ed25519Sign(prvkey: Uint8Array, msg: string) {
-
+    static Ed25519Sign(seckey: Uint8Array, msg: Uint8Array): Uint8Array {
+        return nacl.sign.detached(msg, seckey);
     }
 
-    static Ed25519Verify(pubkey: Uint8Array, msg: string, sig: Uint8Array) {
-
+    static Ed25519Verify(pubkey: Uint8Array, msg: Uint8Array, sig: Uint8Array): boolean {
+        return nacl.sign.detached.verify(msg, sig, pubkey);
     }
 
-    static VerifyMAC(data: Uint8Array, key: Uint8Array, mac: Uint8Array, length: number) {
+    static VerifyMAC(data: Uint8Array, key: Uint8Array, mac: Uint8Array, length: number): boolean {
+        let calculated_mac = Crypto.Sign(key, data);
+        if (mac.byteLength != length || calculated_mac.byteLength < length) {
+            // throw new Error("Bad MAC length");
+            return false;
+        }
 
+        let a = new Uint8Array(calculated_mac);
+        let b = mac;
+        let result = 0;
+        for (var i = 0; i < mac.byteLength; ++i) {
+            result = result | (a[i] ^ b[i]);
+        }
+
+        if (result !== 0) {
+            //throw new Error("Bad MAC");
+            return false;
+        }
+        return true;
     }
 
-    static DeriveSecrets(input: Uint8Array, salt: Uint8Array, info: string) {
-
+    static DeriveSecrets(input: Buffer, salt: Buffer, info: Buffer) {
+        return Crypto.HKDF(input, salt, info);
     }
 
-    static CalculateMAC(key: Uint8Array, data: Uint8Array) {
-
+    static CalculateMAC(key: Buffer, data: Buffer) {
+        return Crypto.Sign(key, data);
     }
 }
--- src/nnt/thirds/dra/signal-protocol/test.ts
+++ src/nnt/thirds/dra/signal-protocol/test.ts
@@ -23,8 +23,19 @@ function test_digest() {
     assert(r == tgt);
 }
 
+async function test_ecc() {
+    let kp = Crypto.CreateKeyPair();
+    console.log(kp);
+
+    let raw = Buffer.from("dfajfkdskalldsafkd");
+    let a = Crypto.Ed25519Sign(kp.secretKey, raw);
+    let b = Crypto.Ed25519Verify(kp.publicKey, raw, a);
+    assert(b);
+}
+
 export async function test_signal_protocol() {
     // Crypto.Test();
     await test_aes();
     await test_digest();
+    await test_ecc();
 }
