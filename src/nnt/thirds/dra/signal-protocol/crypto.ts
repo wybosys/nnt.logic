d21301b266e3ab99379d1a213239b7042d20ebf7
wybosys <wybosys@gmail.com>
2020-09-09 18:13:41 +0800

    dra

--- src/nnt/thirds/dra/signal-protocol/crypto.ts
+++ src/nnt/thirds/dra/signal-protocol/crypto.ts
@@ -1,4 +1,5 @@
 import crypto = require("crypto");
+import {BinaryLike} from "crypto";
 
 export class Crypto {
 
@@ -24,7 +25,7 @@ export class Crypto {
         return Buffer.concat([b0, b1]).toString('utf8');
     }
 
-    static Sign(key: string, data: string): Buffer {
+    static Sign(key: BinaryLike, data: BinaryLike): Buffer {
         let dig = crypto.createHmac('sha256', key);
         let res = dig.update(data);
         return res.digest();
@@ -36,8 +37,22 @@ export class Crypto {
         return res.digest();
     }
 
-    static async HKDF(input: Uint8Array, salt: Uint8Array, info: string): Promise<any[]> {
-        return null;
+    static async HKDF(input: string, salt: string, info: Buffer): Promise<Buffer[]> {
+        // Specific implementation of RFC 5869 that only returns the first 3 32-byte chunks
+        // XTODO: We dont always need the third chunk, we might skip it
+        let PRK = Crypto.Sign(salt, input);
+        let infoBuffer = new Uint8Array(32 + info.byteLength + 1);
+        let infoArray = new Uint8Array(infoBuffer);
+        infoArray.set(new Uint8Array(info), 32);
+        infoArray[infoArray.length - 1] = 1;
+        let T1 = Crypto.Sign(PRK, infoBuffer.slice(32));
+        infoArray.set(new Uint8Array(T1));
+        infoArray[infoArray.length - 1] = 2;
+        let T2 = Crypto.Sign(PRK, infoBuffer);
+        infoArray.set(new Uint8Array(T2));
+        infoArray[infoArray.length - 1] = 3;
+        let T3 = Crypto.Sign(PRK, infoBuffer);
+        return [T1, T2, T3];
     }
 
     static async CreateKeyPair(prvkey?: Uint8Array): Promise<any> {
