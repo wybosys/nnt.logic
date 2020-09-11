b1bcf7c900086ad0656916e2958f11a4a0de0c60
wybosys <wybosys@gmail.com>
2020-09-11 20:16:08 +0800

    signal-protocol

--- src/nnt/thirds/dra/signal-protocol/protocol.ts
+++ src/nnt/thirds/dra/signal-protocol/protocol.ts
@@ -1,44 +1,119 @@
-import {IndexedObject, toJson, toJsonObject} from "../../../core/kernel";
-import {X25519Key} from "./model";
+import {IndexedObject, toJson, toJsonObject, use} from "../../../core/kernel";
+import {KeyPair} from "./model";
+import {IPodObject} from "../../../core/object";
+import {FixedBuffer32} from "../../../core/buffer";
 
-export class Protocol {
+export abstract class Protocol implements IPodObject {
 
     serialout = (): Buffer => {
-        return Buffer.from(toJson(this), 'utf8');
+        return Buffer.from(toJson(this.toPod()), 'utf8');
     }
 
     serialin = (data: Buffer): boolean => {
         let o = toJsonObject(data.toString('utf8'));
         if (!o)
             return false;
-        for (let k in o) {
-            (<IndexedObject>this)[k] = o;
-        }
+        this.fromPod(o);
         return true;
     }
+
+    abstract toPod(): IndexedObject;
+
+    abstract fromPod(obj: IndexedObject): boolean;
 }
 
 export class WhisperMessage extends Protocol {
-    ephemeralKey: X25519Key;
+    ephemeralKey: KeyPair;
     counter: number;
     previousCounter: number;
     ciphertext: Buffer;
+
+    toPod(): IndexedObject {
+        return {
+            ephemeralKey: this.ephemeralKey.toPod(),
+            counter: this.counter,
+            previousCounter: this.previousCounter,
+            ciphertext: this.ciphertext.toString('base64')
+        };
+    }
+
+    fromPod(obj: IndexedObject): boolean {
+        this.ephemeralKey = use(new KeyPair(), kp => {
+            kp.fromPod(obj.ephemeralKey);
+        });
+        this.counter = obj.counter;
+        this.previousCounter = obj.previousCounter;
+        this.ciphertext = Buffer.from(obj.ciphertext, 'base64');
+        return true;
+    }
 }
 
 export class PreKeyWhisperMessage extends Protocol {
     registrationId: number;
     preKeyId: number;
     signedPreKeyId: number;
-    baseKey: X25519Key;
-    identityKey: X25519Key;
+    baseKey: KeyPair;
+    identityKey: KeyPair;
     message: Buffer;
+
+    toPod(): IndexedObject {
+        return {
+            registrationId: this.registrationId,
+            preKeyId: this.preKeyId,
+            signedPreKeyId: this.signedPreKeyId,
+            baseKey: this.baseKey.toPod(),
+            identityKey: this.identityKey.toPod(),
+            message: this.message.toString('base64')
+        };
+    }
+
+    fromPod(obj: IndexedObject): boolean {
+        this.registrationId = obj.registrationId;
+        this.preKeyId = obj.preKeyId;
+        this.signedPreKeyId = obj.signedPreKeyId;
+        this.baseKey = use(new KeyPair(), kp => {
+            kp.fromPod(obj.baseKey);
+        });
+        this.identityKey = use(new KeyPair(), kp => {
+            kp.fromPod(obj.identityKey);
+        });
+        this.message = Buffer.from(obj.message, 'base64');
+        return true;
+    }
 }
 
 export class KeyExchangeMessage extends Protocol {
     id: number;
-    baseKey: Uint8Array;
-    ephemeralKey: Uint8Array;
-    identityKey: Uint8Array;
-    baseKeySignature: Uint8Array;
+    baseKey: KeyPair;
+    ephemeralKey: KeyPair;
+    identityKey: KeyPair;
+    baseKeySignature: FixedBuffer32;
+
+    toPod(): IndexedObject {
+        return {
+            id: this.id,
+            baseKey: this.baseKey.toPod(),
+            ephemeralKey: this.ephemeralKey.toPod(),
+            identityKey: this.identityKey.toPod(),
+            baseKeySignature: this.baseKeySignature.serialize()
+        };
+    }
+
+    fromPod(obj: IndexedObject): boolean {
+        this.id = obj.id;
+        this.baseKey = use(new KeyPair(), kp => {
+            kp.fromPod(obj.baseKey);
+        });
+        this.ephemeralKey = use(new KeyPair(), kp => {
+            kp.fromPod(obj.ephemeralKey);
+        });
+        this.identityKey = use(new KeyPair(), kp => {
+            kp.fromPod(obj.identityKey);
+        });
+        this.baseKeySignature = use(new FixedBuffer32(), buf => {
+            buf.unserialize(obj.baseKeySignature);
+        });
+        return true;
+    }
 }
 
