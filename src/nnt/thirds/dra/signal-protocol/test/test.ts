7ef8b23d60e6d3c1cb21cf71bf0bec14f4cfb1fa
wybosys <wybosys@gmail.com>
2020-09-14 17:27:07 +0800

    test

--- src/nnt/thirds/dra/signal-protocol/test/test.ts
+++ src/nnt/thirds/dra/signal-protocol/test/test.ts
@@ -3,6 +3,7 @@ import {test_keyhelper} from "./test_keyhelper";
 import {test_fingerprint} from "./test_fingerprint";
 import {test_prekeystore} from "./test_prekeystore";
 import {test_address} from "./test_address";
+import {test_chat} from "./test_chat";
 
 export async function test_signal_protocol() {
     test_crypto();
@@ -12,4 +13,5 @@ export async function test_signal_protocol() {
     // test_sessioncipher();
     // test_sessionbuilder();
     test_address();
+    test_chat();
 }
